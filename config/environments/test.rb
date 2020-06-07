842b89c738c56e287486c3685a0cc64117df2925
mitsuba19930328 <mitsuba19930328@gmail.com>
2020-06-07 05:40:20 +0000

    before 11.3 アカウントを有効化する

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: '0a40516caf78418495510c44a3ce42e3.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
